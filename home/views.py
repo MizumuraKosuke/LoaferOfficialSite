2cb0e6be002a461c19d8279940b7caf06f50f2a9
MizumuraKosuke <vocal.loafer@gmail.com>
2018-05-26 17:03:18 +0900

    home

--- home/views.py
+++ home/views.py
@@ -3,7 +3,9 @@ from django.utils import timezone
 import os,sys
 sys.path.append(os.pardir)
 from live.models import Live
+from news.models import News
 
 def home(request):
     next_lives = Live.objects.filter(published_date__lte=timezone.now()).order_by('date').reverse().filter(date__gte=timezone.now())
-    return render(request, 'home/home.html', {'next_lives': next_lives})
+    newss = News.objects.filter(published_date__lte=timezone.now()).order_by('published_date').reverse()
+    return render(request, 'home/home.html', {'next_lives': next_lives, 'newss': newss})
