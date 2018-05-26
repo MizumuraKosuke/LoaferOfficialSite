91c2d424a7a6e6cee65894e64c76744041ef8d6b
MizumuraKosuke <vocal.loafer@gmail.com>
2018-05-26 17:47:10 +0900

    js

---
+++ staticfiles/js/base.js
@@ -0,0 +1,41 @@
+var image = new Array();
+
+image[0] = new Image();
+image[0].src = "/../static/media/news/image.png";
+image[1] = new Image();
+image[1].src = "/../static/media/news/image-2.png";
+
+var cnt=0;
+
+var timer1;
+
+function slideNews(){
+    cnt++;
+    if (cnt >= 2) {
+        cnt = 0;
+    }
+    document.getElementById("sd").src = image[cnt].src;
+    timer1 = setTimeout("slideNews()",5000);
+}
+
+function forwardNews(){
+    clearTimeout(timer1);
+    cnt++;
+    if (cnt >= 2) {
+        cnt = 0;
+    }
+    document.getElementById("sd").src = image[cnt].src;
+    timer1 = setTimeout("slideNews()",5000);
+}
+
+function backNews(){
+    clearTimeout(timer1);
+    cnt--;
+    if (cnt <= 0) {
+        cnt = 1;
+    }
+    document.getElementById("sd").src = image[cnt].src;
+    timer1 = setTimeout("slideNews()",5000);
+}
+
+slideNews();
